5780a237763dcd8d9cb232de227e8fbb00311d72
Abulkosim <abulkasimkattaev@gmail.com>
2025-06-20 00:58:48 +0500

    crud in mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
+import fs from "fs/promises";
+
 dotenv.config();
 
 async function main() {
@@ -12,17 +14,19 @@ async function main() {
 
   const client = new MongoClient(uri);
   await client.connect();
-  const databases: any = await getAllDatabases(client);
 
-  for (const database of databases) {
-    console.log("database name: ", database.name);
-    const collections = await getCollection(client, database.name);
-    for (const collection of collections) {
-      console.log("collection name: ", collection.name);
-    }
+  const collections = await getCollection(client, "sample_mflix")
+
+  const fileExists = await fs.stat('src/collections.json').then(() => true).catch(() => false);
+  if (!fileExists) {
+    await fs.writeFile('src/collections.json', JSON.stringify(collections, null, 2))
+  } else {
+    console.log("collections.json already exists");
   }
 
-  await client.db("sample_supplies").dropDatabase();
+  const users = await getCollectionData(client, "sample_mflix", "users")
+  await resultSaver(users, "users")
+
   await client.close();
 }
 main().catch(console.error);
@@ -32,8 +36,24 @@ async function getAllDatabases(client: MongoClient) {
   return databasesList.databases;
 }
 
+async function resultSaver(data: any, filename: string) {
+  const fileExists = await fs.stat(`src/${filename}.json`).then(() => true).catch(() => false);
+  if (!fileExists) {
+    await fs.writeFile(`src/${filename}.json`, JSON.stringify(data, null, 2))
+  } else {
+    console.log(`${filename}.json already exists`);
+  }
+}
+
 async function getCollection(client: MongoClient, databaseName: string) {
   const db = client.db(databaseName);
   const collections = await db.listCollections().toArray();
   return collections;
+}
+
+async function getCollectionData(client: MongoClient, databaseName: string, collectionName: string) {
+  const db = client.db(databaseName);
+  const collection = db.collection(collectionName);
+  const data = await collection.find({ name: "Ned Stark"}).toArray();
+  return data;
 }
